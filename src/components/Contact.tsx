c564b27bb60afc2a6a07b71bd7e72d64075734aa
3dgar21 <delangeledgar34@gmail.com>
2025-06-18 15:58:43 -0400

    instagram updated

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -55,7 +55,7 @@ export default function Contact() {
               <FaFacebook size={24} />
             </a>
             <a
-              href="https://www.instagram.com/islandgutter"
+              href="https://www.instagram.com/island_gutter/"
               target="_blank"
               rel="noopener noreferrer"
               className="text-muted-foreground hover:text-primary"
