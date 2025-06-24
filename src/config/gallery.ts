8d4d755d98727c0fec415ed3e0c33b45399c94f4
3dgar21 <delangeledgar34@gmail.com>
2025-06-24 14:40:43 -0400

    update6.24

--- src/config/gallery.ts
+++ src/config/gallery.ts
@@ -1,65 +1,324 @@
 export type GalleryImage = {
   src: string;
   alt: string;
-  category: 'Gutters' | 'Decks' | 'Siding' | 'Concrete & Masonry Work' | 'Before & After' | 'bathrooms' | 'Kitchens' | 'Roofing'| 'Other';
+  category: 'Gutters & Roofing' | 'Decks & Siding' | 'Concrete & Masonry Work' | 'Doors & Window' | 'Bathrooms & Kitchens' | 'Other';
 };
 
 export const galleryImages: GalleryImage[] = [
   {
-    src: '/gallery/before1.jpeg',
-    alt: 'Gutter installation',
-    category: 'Before & After',
-  },
-  {
-    src: '/gallery/after1.jpeg',
-    alt: 'Gutter installation',
-    category: 'Before & After',
-  },
-  {
-    src: '/gallery/bathroom1.jpeg',
+    src: '/gallery/b&k/bathroom1.jpeg',
     alt: 'Bathroom renovation project',
-    category: 'bathrooms',
+    category: 'Bathrooms & Kitchens',
   },
   {
-    src: '/gallery/bathroom2.jpeg',
+    src: '/gallery/b&k/bathroom2.jpeg',
     alt: 'Bathroom renovation project',
-    category: 'bathrooms',
+    category: 'Bathrooms & Kitchens',
   },
   {
-    src: '/gallery/bathroom3.jpeg',
+    src: '/gallery/b&k/bathroom3.jpeg',
     alt: 'bathroom renovation project',
-    category: 'bathrooms',
+    category: 'Bathrooms & Kitchens',
     
   },
    {
-    src: '/gallery/bathroom4.jpeg',
+    src: '/gallery/b&k/bathroom4.jpeg',
     alt: 'bathroom renovation project',
-    category: 'bathrooms',
+    category: 'Bathrooms & Kitchens',
     
   },
 
   {
-    src: '/gallery/Concrete1.jpeg',
+    src: '/gallery/concrete/Concrete1.jpeg',
     alt: 'Concrete and masonry work',
     category: 'Concrete & Masonry Work',
     
   },
   {
-    src: '/gallery/Concrete2.jpeg',
+    src: '/gallery/concrete/Concrete2.jpeg',
     alt: 'Concrete and masonry work',
     category: 'Concrete & Masonry Work',
     
   },
 
    {
-    src: '/gallery/sidding1.jpeg',
+    src: '/gallery/s&d/pic1.jpeg',
     alt: 'sidding installation',
-    category: 'Siding',
+    category: 'Decks & Siding',
   },
   {
-    src: '/gallery/sidding2.jpeg',
+    src: '/gallery/s&d/pic2.jpeg',
     alt: 'sidding installation',
-    category: 'Siding',
+    category: 'Decks & Siding',
+  },
+  {
+    src: '/gallery/concrete/pic1.JPG',
+    alt: 'Concrete and masonry work',
+    category: 'Concrete & Masonry Work',
+  },
+  {
+    src: '/gallery/concrete/pic2.JPG',
+    alt: 'Concrete and masonry work',
+    category: 'Concrete & Masonry Work',
+  },
+  {
+    src: '/gallery/concrete/pic3.JPG',
+    alt: 'Concrete and masonry work',
+    category: 'Concrete & Masonry Work',
+  },
+  {
+    src: '/gallery/concrete/pic4.JPG',
+    alt: 'Concrete and masonry work',
+    category: 'Concrete & Masonry Work',
+  },
+  {
+    src: '/gallery/concrete/pic5.JPG',
+    alt: 'Concrete and masonry work',
+    category: 'Concrete & Masonry Work',
+  },
+  {
+    src: '/gallery/concrete/pic6.JPG',
+    alt: 'Concrete and masonry work',
+    category: 'Concrete & Masonry Work',
+  },
+  {
+    src: '/gallery/concrete/pic7.JPG',
+    alt: 'Concrete and masonry work',
+    category: 'Concrete & Masonry Work',
+  },
+  {
+    src: '/gallery/concrete/pic8.JPG',
+    alt: 'Concrete and masonry work',
+    category: 'Concrete & Masonry Work',
+  },
+  {
+    src: '/gallery/concrete/pic9.jpeg',
+    alt: 'Concrete and masonry work',
+    category: 'Concrete & Masonry Work',
+  },
+  {
+    src: '/gallery/s&d/pic3.jpeg',
+    alt: 'sidding installation',
+    category: 'Decks & Siding',
+  },
+  {
+    src: '/gallery/s&d/pic4.jpeg',
+    alt: 'sidding installation',
+    category: 'Decks & Siding',
+  },
+  {
+    src: '/gallery/s&d/pic5.jpeg',
+    alt: 'sidding installation',
+    category: 'Decks & Siding',
+  },
+  {
+    src: '/gallery/s&d/pic6.jpeg',
+    alt: 'sidding installation',
+    category: 'Decks & Siding',
+  },
+  {
+    src: '/gallery/b&k/pic1.jpeg',
+    alt: 'bathroom renovation project',
+    category: 'Bathrooms & Kitchens',
+    
+  },
+  {
+    src: '/gallery/b&k/pic2.jpeg',
+    alt: 'bathroom renovation project',
+    category: 'Bathrooms & Kitchens',
+    
+  },
+  {
+    src: '/gallery/b&k/pic4.jpeg',
+    alt: 'bathroom renovation project',
+    category: 'Bathrooms & Kitchens',
+    
+  },
+  {
+    src: '/gallery/b&k/pic5.jpeg',
+    alt: 'bathroom renovation project',
+    category: 'Bathrooms & Kitchens',
+    
+  },
+  {
+    src: '/gallery/d&w/pic1.jpeg',
+    alt: 'Door and window installation',
+    category: 'Doors & Window',
+  },
+  {
+    src: '/gallery/d&w/pic2.jpeg',
+    alt: 'Door and window installation',
+    category: 'Doors & Window',
+  },{
+    src: '/gallery/d&w/pic3.jpeg',
+    alt: 'Door and window installation',
+    category: 'Doors & Window',
+  },{
+    src: '/gallery/d&w/pic3.jpeg',
+    alt: 'Door and window installation',
+    category: 'Doors & Window',
+  },{
+    src: '/gallery/d&w/pic4.jpeg',
+    alt: 'Door and window installation',
+    category: 'Doors & Window',
+  },{
+    src: '/gallery/d&w/pic5.jpeg',
+    alt: 'Door and window installation',
+    category: 'Doors & Window',
+  },{
+    src: '/gallery/d&w/pic6.jpeg',
+    alt: 'Door and window installation',
+    category: 'Doors & Window',
+  },{
+    src: '/gallery/d&w/pic7.jpeg',
+    alt: 'Door and window installation',
+    category: 'Doors & Window',
+  },{
+    src: '/gallery/d&w/pic8.jpeg',
+    alt: 'Door and window installation',
+    category: 'Doors & Window',
+  },{
+    src: '/gallery/d&w/pic9.jpeg',
+    alt: 'Door and window installation',
+    category: 'Doors & Window',
+  },{
+    src: '/gallery/d&w/pic10.jpeg',
+    alt: 'Door and window installation',
+    category: 'Doors & Window',
+  },{
+    src: '/gallery/d&w/pic11.jpeg',
+    alt: 'Door and window installation',
+    category: 'Doors & Window',
+  },
+  {
+    src: '/gallery/d&w/pic12.jpeg',
+    alt: 'Door and window installation',
+    category: 'Doors & Window',
+  },
+  {
+    src: '/gallery/g&r/pic1.jpeg',
+    alt: 'Gutter and roofing installation',
+    category: 'Gutters & Roofing',
+  },
+   {
+    src: '/gallery/g&r/pic2.jpeg',
+    alt: 'Gutter and roofing installation',
+    category: 'Gutters & Roofing',
+  },
+   {
+    src: '/gallery/g&r/pic3.jpeg',
+    alt: 'Gutter and roofing installation',
+    category: 'Gutters & Roofing',
+  },
+   {
+    src: '/gallery/g&r/pic4.jpeg',
+    alt: 'Gutter and roofing installation',
+    category: 'Gutters & Roofing',
+  }, {
+    src: '/gallery/g&r/pic5.jpeg',
+    alt: 'Gutter and roofing installation',
+    category: 'Gutters & Roofing',
+  }, {
+    src: '/gallery/g&r/pic6.jpeg',
+    alt: 'Gutter and roofing installation',
+    category: 'Gutters & Roofing',
+  }, {
+    src: '/gallery/g&r/pic7.jpeg',
+    alt: 'Gutter and roofing installation',
+    category: 'Gutters & Roofing',
+  }, {
+    src: '/gallery/g&r/pic8.jpeg',
+    alt: 'Gutter and roofing installation',
+    category: 'Gutters & Roofing',
+  }, {
+    src: '/gallery/g&r/pic9.jpeg',
+    alt: 'Gutter and roofing installation',
+    category: 'Gutters & Roofing',
+  }, {
+    src: '/gallery/g&r/pic10.jpeg',
+    alt: 'Gutter and roofing installation',
+    category: 'Gutters & Roofing',
+  }, {
+    src: '/gallery/g&r/pic11.jpeg',
+    alt: 'Gutter and roofing installation',
+    category: 'Gutters & Roofing',
+  }, {
+    src: '/gallery/g&r/pic12.jpeg',
+    alt: 'Gutter and roofing installation',
+    category: 'Gutters & Roofing',
+  }, {
+    src: '/gallery/g&r/pic13.jpeg',
+    alt: 'Gutter and roofing installation',
+    category: 'Gutters & Roofing',
+  }, {
+    src: '/gallery/g&r/pic14.jpeg',
+    alt: 'Gutter and roofing installation',
+    category: 'Gutters & Roofing',
+  }, {
+    src: '/gallery/g&r/pic15.jpeg',
+    alt: 'Gutter and roofing installation',
+    category: 'Gutters & Roofing',
+  }, {
+    src: '/gallery/g&r/pic16.jpeg',
+    alt: 'Gutter and roofing installation',
+    category: 'Gutters & Roofing',
+  }, {
+    src: '/gallery/g&r/pic17.jpeg',
+    alt: 'Gutter and roofing installation',
+    category: 'Gutters & Roofing',
+  }, {
+    src: '/gallery/g&r/pic18.jpeg',
+    alt: 'Gutter and roofing installation',
+    category: 'Gutters & Roofing',
+  }, {
+    src: '/gallery/g&r/pic19.jpeg',
+    alt: 'Gutter and roofing installation',
+    category: 'Gutters & Roofing',
+  }, {
+    src: '/gallery/g&r/pic20.jpeg',
+    alt: 'Gutter and roofing installation',
+    category: 'Gutters & Roofing',
+  }, {
+    src: '/gallery/g&r/pic21.jpeg',
+    alt: 'Gutter and roofing installation',
+    category: 'Gutters & Roofing',
+  }, {
+    src: '/gallery/g&r/pic22.jpeg',
+    alt: 'Gutter and roofing installation',
+    category: 'Gutters & Roofing',
+  }, {
+    src: '/gallery/g&r/pic23.jpeg',
+    alt: 'Gutter and roofing installation',
+    category: 'Gutters & Roofing',
+  }, {
+    src: '/gallery/g&r/pic24.jpeg',
+    alt: 'Gutter and roofing installation',
+    category: 'Gutters & Roofing',
+  },
+  {
+    src: '/gallery/g&r/pic25.jpeg',
+    alt: 'Gutter and roofing installation',
+    category: 'Gutters & Roofing',
+  },
+  {
+    src: '/gallery/g&r/pic26.jpeg',
+    alt: 'Gutter and roofing installation',
+    category: 'Gutters & Roofing',
+  },
+  {
+    src: '/gallery/g&r/pic27.jpeg',
+    alt: 'Gutter and roofing installation',
+    category: 'Gutters & Roofing',
+  },
+  {
+    src: '/gallery/g&r/pic28.jpeg',
+    alt: 'Gutter and roofing installation',
+    category: 'Gutters & Roofing',
+  },
+  {
+    src: '/gallery/g&r/pic29.jpeg',
+    alt: 'Gutter and roofing installation',
+    category: 'Gutters & Roofing',
   },
   // Add the rest here...
 ];
